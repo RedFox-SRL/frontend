2d3949c024542d3e7b1e67166feba50694990614
Catiel <oliver.alandiaa@gmail.com>
2024-09-27 13:52:41 -0400

    add new currentView

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import Layout from '../components/Layout'
 import Dashboard from '../components/Dashboard'
 import GruposYPlanificacion from '../components/GruposYPlanificacion'
 import Perfil from '../components/Perfil'
+import TeamsTable from "@/components/TeamsTable";
 
 export default function StudentDashboard() {
-  const [currentView, setCurrentView] = useState('inicio')
+    const [currentView, setCurrentView] = useState('inicio')
 
-  const renderContent = () => {
-    switch (currentView) {
-      case 'inicio':
-        return <Dashboard />
-      case 'grupo':
-        return <GruposYPlanificacion />
-      case 'perfil':
-        return <Perfil />
-      default:
-        return <Dashboard />
+    const renderContent = () => {
+        switch (currentView) {
+            case 'inicio':
+                return <Dashboard/>
+            case 'grupo':
+                return <GruposYPlanificacion/>
+            case 'perfil':
+                return <Perfil/>
+            case 'empresas':
+                return <TeamsTable/>
+            default:
+                return <Dashboard/>
+        }
     }
-  }
 
-  return (
-    <Layout setCurrentView={setCurrentView}>
-      {renderContent()}
-    </Layout>
-  )
+    return (
+        <Layout setCurrentView={setCurrentView}>
+            {renderContent()}
+        </Layout>
+    )
 }
