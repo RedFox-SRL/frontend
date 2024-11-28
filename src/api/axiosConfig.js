603fbe6969953d6e0c4e8c3c37097151d9f3a9d5
Mijael Oliver Alandia Vargas <136261261+Catiel@users.noreply.github.com>
2024-11-28 17:46:03 -0400

    Update axiosConfig.js
    Signed-off-by: Mijael Oliver Alandia Vargas <136261261+Catiel@users.noreply.github.com>

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://trackmaster-ct62m.ondigitalocean.app/api',
+  baseURL: 'https://trackmaster-avw68.ondigitalocean.app/api',
 });
 
 api.interceptors.request.use(
