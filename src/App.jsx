7bdc25c30146573a7c8da88d4c9a4d4f967507b6
Catiel <oliver.alandiaa@gmail.com>
2024-12-03 21:48:17 -0400

    fddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,63 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import LoginRegister from "./pages/LoginRegister";
 import LoginPage from "./pages/LoginPage";
 import StudentDashboard from "./pages/StudentDashboard";
 import TeacherDashboard from "./pages/TeacherDashboard";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import { AuthProvider } from "./context/AuthContext";
+import {AuthProvider} from "./context/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import UnauthorizedPage from "./pages/UnauthorizedPage.jsx";
 import PublicRoute from "./routes/PublicRoute.jsx";
-import { Toaster } from "@/components/ui/toaster";
+import {Toaster} from "@/components/ui/toaster";
 
 function App() {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          {/* Rutas públicas */}
-          <Route
-            path="/"
-            element={
-              <PublicRoute>
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <PublicRoute>
-                <LoginRegister />
-              </PublicRoute>
-            }
-          />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route
-            path="/DashboardStudent"
-            element={
-              <ProtectedRoute requiredRole="student">
-                <StudentDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/DashboardTeacher"
-            element={
-              <ProtectedRoute requiredRole="teacher">
-                <TeacherDashboard />
-              </ProtectedRoute>
-            }
-          />
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    {/* Rutas públicas */}
+                    <Route
+                        path="/"
+                        element={
+                            <PublicRoute>
+                                <LoginPage/>
+                            </PublicRoute>
+                        }
+                    />
+                    <Route
+                        path="/register"
+                        element={
+                            <PublicRoute>
+                                <LoginRegister/>
+                            </PublicRoute>
+                        }
+                    />
+                    <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+                    <Route
+                        path="/DashboardStudent"
+                        element={
+                            <ProtectedRoute requiredRole="student">
+                                <StudentDashboard/>
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/DashboardTeacher"
+                        element={
+                            <ProtectedRoute requiredRole="teacher">
+                                <TeacherDashboard/>
+                            </ProtectedRoute>
+                        }
+                    />
 
-          <Route path="/accessDenied" element={<UnauthorizedPage />} />
-        </Routes>
-        <Toaster />
-      </AuthProvider>
-    </Router>
-  );
+                    <Route path="/accessDenied" element={<UnauthorizedPage/>}/>
+                    <Route path="*" element={<Navigate to="/" replace/>}/>
+                </Routes>
+                <Toaster/>
+            </AuthProvider>
+        </Router>
+    );
 }
 
 export default App;
